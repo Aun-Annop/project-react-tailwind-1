aba3999cb26ddcf832687dc9c54be14da075e0be
Annop Sukprasert <144894213+Aun-Annop@users.noreply.github.com>
2023-09-17 14:14:18 +0700

    Add files via upload

---
+++ src/data/data.js
@@ -0,0 +1,169 @@
+export const data =[
+    {
+        id: 1,
+        name: 'Mango blended two colors smoothie',
+        category: 'Mango',
+        image:'https://media.istockphoto.com/id/1450037480/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%AD%E0%B8%A3%E0%B9%88%E0%B8%AD%E0%B8%A2%E0%B8%A1%E0%B8%B0%E0%B8%A1%E0%B9%88%E0%B8%A7%E0%B8%87%E0%B8%AA%E0%B8%AD%E0%B8%87%E0%B8%AA%E0%B8%B5%E0%B8%9B%E0%B8%B1%E0%B8%99%E0%B8%99%E0%B8%B2%E0%B8%84%E0%B8%AD%E0%B8%95%E0%B8%95%E0%B9%89%E0%B8%B2%E0%B8%A1%E0%B8%B9%E0%B8%AA%E0%B8%9E%E0%B8%B8%E0%B8%94%E0%B8%94%E0%B8%B4%E0%B9%89%E0%B8%87%E0%B8%9A%E0%B8%99%E0%B8%9E%E0%B8%B7%E0%B9%89%E0%B8%99%E0%B8%AB%E0%B8%A5%E0%B8%B1%E0%B8%87%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0%E0%B9%84%E0%B8%A1%E0%B9%89.jpg?s=612x612&w=is&k=20&c=tX9X50sxcf5EBplgOF75bCtgYnc90Vag_uEMZ96Tzho=',
+        price: '$10',
+    },
+    {
+        id: 2,
+        name: 'Mango blended',
+        category: 'Mango',
+        image:'https://media.istockphoto.com/id/1077607676/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%A1%E0%B8%B0%E0%B8%A1%E0%B9%88%E0%B8%A7%E0%B8%87-lassi-%E0%B9%82%E0%B8%A2%E0%B9%80%E0%B8%81%E0%B8%B4%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%AB%E0%B8%A3%E0%B8%B7%E0%B8%AD%E0%B8%AA%E0%B8%A1%E0%B8%B9%E0%B8%97%E0%B8%95%E0%B8%B5%E0%B9%89%E0%B8%81%E0%B8%B1%E0%B8%9A%E0%B8%82%E0%B8%A1%E0%B8%B4%E0%B9%89%E0%B8%99-%E0%B9%80%E0%B8%84%E0%B8%A3%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%87%E0%B8%94%E0%B8%B7%E0%B9%88%E0%B8%A1%E0%B8%A4%E0%B8%94%E0%B8%B9%E0%B8%A3%E0%B9%89%E0%B8%AD%E0%B8%99%E0%B9%80%E0%B8%A2%E0%B9%87%E0%B8%99%E0%B8%AD%E0%B8%B4%E0%B8%99%E0%B9%80%E0%B8%94%E0%B8%B5%E0%B8%A2%E0%B9%82%E0%B8%9B%E0%B8%A3%E0%B9%84%E0%B8%9A%E0%B9%82%E0%B8%AD%E0%B8%95%E0%B8%B4%E0%B8%81%E0%B9%80%E0%B8%9E%E0%B8%B7%E0%B9%88%E0%B8%AD%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2.jpg?s=612x612&w=is&k=20&c=Ilj7YevXn4GQj4pFls0RbwdmQJa25z8nKAj_-s4-1c0=',
+        price: '$40',
+    },
+    {
+        id: 3,
+        name: 'Mango juice',
+        category: 'Mango',
+        image:'https://media.istockphoto.com/id/1141482394/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%99%E0%B9%89%E0%B9%8D%E0%B8%B2%E0%B8%A1%E0%B8%B0%E0%B8%A1%E0%B9%88%E0%B8%A7%E0%B8%87%E0%B9%83%E0%B8%99%E0%B9%81%E0%B8%81%E0%B9%89%E0%B8%A7%E0%B8%9A%E0%B8%99%E0%B9%81%E0%B8%9C%E0%B9%88%E0%B8%99%E0%B8%95%E0%B8%B1%E0%B8%94.jpg?s=612x612&w=is&k=20&c=HXxObojAEWsZa1QbPpS7aMv_RgblWM1maeXbT8JwAQU=',
+        price: '$30',
+    },
+    {
+        id: 4,
+        name: 'Green tea blended',
+        category: 'Green tea',
+        image:'https://images.unsplash.com/photo-1611497426695-412abe2f287b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1040&q=80',
+        price: '$20',
+    },
+    {
+        id: 5,
+        name: 'Green tea',
+        category: 'Green tea',
+        image:'https://media.istockphoto.com/id/914711582/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%8A%E0%B8%B2%E0%B9%80%E0%B8%82%E0%B8%B5%E0%B8%A2%E0%B8%A7%E0%B9%80%E0%B8%A2%E0%B9%87%E0%B8%99%E0%B8%9A%E0%B8%99%E0%B9%82%E0%B8%95%E0%B9%8A%E0%B8%B0.jpg?s=612x612&w=is&k=20&c=Hk2zcqQH7DysAKipkmfVnL3BcazExpY4erpbh7k4xEM=',
+        price: '$10',
+    },
+    {
+        id: 6,
+        name: 'Green tea with lemon',
+        category: 'Green tea',
+        image:'https://media.istockphoto.com/id/499808366/th/%E0%B8%A3%E0%B8%B9%E0%B8%9B%E0%B8%96%E0%B9%88%E0%B8%B2%E0%B8%A2/%E0%B8%99%E0%B9%89%E0%B9%8D%E0%B8%B2%E0%B8%9C%E0%B8%A5%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B9%81%E0%B8%A5%E0%B8%B0%E0%B8%9C%E0%B8%A5%E0%B9%84%E0%B8%A1%E0%B9%89%E0%B8%AB%E0%B8%A7%E0%B8%B2%E0%B8%99.jpg?s=612x612&w=0&k=20&c=ZWkPp8t3c-MYgizU3vMczy5G39TMQrDxDRvzP0Kh9Vo=',
+        price: '$40',
+    },
+    {
+        id: 7,
+        name: 'Strawberry Smoothie',
+        category: 'Strawberry',
+        image:'https://images.unsplash.com/photo-1669277038674-9d00b4ff41fa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        price: '$30',
+    },
+    {
+        id: 8,
+        name: 'Strawberry Cherry Smoothie',
+        category: 'Strawberry',
+        image:'https://images.unsplash.com/photo-1553530666-ba11a7da3888?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=986&q=80',
+        price: '$20',
+    },
+    {
+        id: 9,
+        name: 'Strawberry blueberry fresh milk',
+        category: 'Strawberry',
+        image:'https://images.unsplash.com/photo-1600718374662-0483d2b9da44?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=985&q=80',
+        price: '$10',
+    },
+    {
+        id: 10,
+        name: 'Oreo Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1612203985729-70726954388c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8RGVzc2VydHN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60',
+        price: '$40',
+    },
+    {
+        id: 10,
+        name: 'Strawberry Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1587314168485-3236d6710814?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1078&q=80',
+        price: '$30',
+    },
+    {
+        id: 11,
+        name: 'Ice cream Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1624353365286-3f8d62daad51?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        price: '$20',
+    },
+    {
+        id: 12,
+        name: 'Macarons Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1675166640038-c1f67c27bd0d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXxQLWpIc2ZqR202VXx8ZW58MHx8fHx8&auto=format&fit=crop&w=500&q=60',
+        price: '$10',
+    },
+    {
+        id: 13,
+        name: 'Cake Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1642974369605-0a2d037eee00?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        price: '$40',
+    },
+    {
+        id: 14,
+        name: 'Cake Dark Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1625932948487-10d58571c487?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80',
+        price: '$30',
+    },
+    {
+        id: 15,
+        name: 'Ice cream Dark Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDJ8fHxlbnwwfHx8fHw%3D&auto=format&fit=crop&w=500&q=60',
+        price: '$20',
+    },
+    {
+        id: 16,
+        name: 'Hart Dark Desserts',
+        category: 'Desserts',
+        image:'https://images.unsplash.com/photo-1582654199128-c8a6e33aef3a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',
+        price: '$10',
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        name: 'Dessert',
+        image:'https://cdn.icon-icons.com/icons2/881/PNG/512/Cupcake_icon-icons.com_68756.png',
+    },
+    {
+        id: 2,
+        name: 'Cream',
+        image:'https://png.pngtree.com/png-vector/20221021/ourmid/pngtree-colorful-ice-cream-icon-png-image_6377439.png',
+    },
+    {
+        id: 3,
+        name: 'Fruit juice',
+        image:'https://cdn-icons-png.flaticon.com/512/4163/4163589.png',
+    },
+    {
+        id: 4,
+        name: 'Tea blended',
+        image:'https://png.pngtree.com/png-vector/20220629/ourmid/pngtree-banana-kiwi-smoothie-icon-png-image_5592852.png',
+    },
+    {
+        id: 5,
+        name: 'Latest Deals',
+        image:
+          'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Deals_BrowseHome@3x.png',
+      },
+      {
+        id: 6,
+        name: 'Restaurant Rewards',
+        image:
+          'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/RestoRewards_BrowseHome@3x.png',
+      },
+      {
+        id: 7,
+        name: 'Best Overall',
+        image:
+          'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/TopEats_Browse%20Home@3x.png',
+      },
+      {
+        id: 8,
+        name: 'Shipped Free',
+        image:
+          'https://duyt4h9nfnj50.cloudfront.net/new_search_home_eats_icon/Placeholder_Plates@3x.png',
+      },
+
+];
